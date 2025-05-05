8320200437b16cfcc3174c7a2a81e7423db82fe5
David Given <dg@cowlark.com>
2025-05-05 19:26:05 +0200

    Refactor the engine not to be interrupt-driven.

--- src/midinote.py
+++ src/midinote.py
@@ -1,33 +1,32 @@
 from samplerate import SAMPLE_FREQ, OCTAVES
 import math
 
-print(
-    """
+print("""
 #include "zif.inc"
 zproc note_table
-"""
-)
+""")
 
 # Calculates the mapping between our internal note numbers (one third of a
 # semitone with 0 being MIDI note 24) and the intervals that the timer is
 # programmed with.
+
+TRANSPOSE = 0
+
 def period(n):
-    freq = (2 ** ((n - 69) / 12)) * 440
+    freq = (2 ** ((n-69)/12))*440
     return int(SAMPLE_FREQ / freq)
 
-
-for n in range(0, OCTAVES * 12 * 3):
-    midinote = 24 + (n / 3)
-    freq = (2 ** ((midinote - 69) / 12)) * 440
+for n in range(0, OCTAVES*12*3):
+    n = n + TRANSPOSE
+    midinote = 24 + (n/3)
+    freq = (2 ** ((midinote-69)/12))*440
     period = int(round(SAMPLE_FREQ / freq))
 
-    realfreq = SAMPLE_FREQ / period
-    realmidi = 69 + 12 * math.log2(realfreq / 440)
+    realfreq = SAMPLE_FREQ/period
+    realmidi = 69 + 12*math.log2(realfreq / 440)
 
     if (n % 3) == 0:
-        print(
-            f" .byte {period} ; freq {freq:.1f}Hz midi note {midinote}, error {(midinote-realmidi)*100:.2f} cents"
-        )
+        print(f" .byte {period} ; freq {freq:.1f}Hz midi note {midinote}, error {(midinote-realmidi)*100:.2f} cents")
     else:
         print(f" .byte {period}")
 
--- src/samplerate.py
+++ src/samplerate.py
@@ -1,4 +1,4 @@
 # See utils/notecalc.py on where this extremely magic number came from.
 
-SAMPLE_FREQ = 7911
-OCTAVES = 5
+SAMPLE_FREQ = 8101
+OCTAVES = 6
